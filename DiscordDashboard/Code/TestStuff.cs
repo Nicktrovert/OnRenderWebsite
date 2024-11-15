511782867dad3a5acaa328f0e41b9fed75a3e197
Nick Helmers <yolorolo2007@gmail.com>
2024-11-15 16:27:20 +0100

    Projektdateien hinzufügen.

---
+++ DiscordDashboard/Code/TestStuff.cs
@@ -0,0 +1,27 @@
+﻿namespace DiscordDashboard.Code
+{
+    public sealed class TestStuff
+    {
+        private TestStuff(){}
+
+        private static TestStuff _instance;
+
+        public static TestStuff GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new TestStuff();
+            }
+            return _instance;
+        }
+
+        private int _counter = 0;
+
+        public int Counter
+        {
+            get => _counter;
+            set => _counter = value;
+        }
+
+    }
+}
